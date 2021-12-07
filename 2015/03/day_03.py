d21d1986d3b3677a58d5f1656a4a98cb13ff5b9f
priit <priit.kylaots@gmail.com>
2021-12-07 08:55:48 +0200

    15/03, 21/07

---
+++ 2015/03/day_03.py
@@ -0,0 +1,37 @@
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+from common import get_input
+from collections import defaultdict
+
+def get_pos(input):
+    x, y = 0, 0
+    yield(x, y)
+    for ch in input:
+        if ch == '^':
+            x += 1
+        elif ch == 'v':
+            x -= 1
+        elif ch == '>':
+            y += 1
+        elif ch == '<':
+            y -= 1
+        yield(x, y)
+
+def part_1(input):
+    D = defaultdict(int)
+    for x, y in get_pos(input):
+        D[(x, y)] += 1
+    return len(D)
+
+def part_2(input):
+    D = defaultdict(int)
+    for x, y in get_pos(''.join([input[2 * i] for i in range(len(input) // 2)])):
+        D[(x, y)] += 1
+    for x, y in get_pos(''.join([input[2 * i + 1] for i in range(len(input) // 2)])):
+        D[(x, y)] += 1
+    return len(D)
+
+if __name__ == '__main__':
+    input  = get_input()
+    print(part_1(input))
+    print(part_2(input))      
---
+++ 2021/07/day_07.py
@@ -0,0 +1,31 @@
+from os import sys, path
+import time
+sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+from common import get_input
+from statistics import median
+
+sample = '''\
+16,1,2,0,4,2,7,1,2,14\
+'''
+
+def part_1():
+    m = int(median(l))
+    return sum(abs(m - i) for i in l)
+
+def part_2():
+    res = []
+    for i in range(min(l), max(l) + 1):
+        fuel = 0
+        for j in l:
+            move = abs(i - j)
+            fuel += int(move * (move + 1) / 2)  
+        res.append(fuel)
+    return min(res)
+
+if __name__ == '__main__':
+    input  = get_input()
+    global l
+    # l = [int(i) for i in sample.split(',')]
+    l = [int(i) for i in input.split(',')]
+    print(part_1())
+    print(part_2())
